4062bce345ae1ad31170b68823fb93de876f62bf
Rick Dumpleton <78909973+rickd-uk@users.noreply.github.com>
2023-01-13 16:16:59 +0900

    add: log util

--- client/src/components/Quiz.js
+++ client/src/components/Quiz.js
@@ -1,5 +1,28 @@
 import React from 'react'
 
+import log from '../utils/output.js'
+
 export default function Quiz() {
-	return <div>Quiz</div>
+	const onNext = () => {
+		log('next')
+	}
+	const onPrev = () => {
+		log('previous')
+	}
+
+	return (
+		<div className='container'>
+			<h1 className='title text-light'>Quiz App</h1>
+			{/** Display Questions */}
+
+			<div className='grid'>
+				<button className='btn prev' onClick={onPrev}>
+					Prev
+				</button>
+				<button className='btn next' onClick={onNext}>
+					Next
+				</button>
+			</div>
+		</div>
+	)
 }
